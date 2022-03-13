69a6f69af1f11121a903da35ba960ef907167d5d
krishna vishwakarma <60446420+kv0871916@users.noreply.github.com>
2022-03-13 17:59:03 +0530

    App: Added Firebase initialize App

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -5,9 +5,16 @@
 // license that can be found in the LICENSE file or at
 // https://opensource.org/licenses/MIT.
 
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
 import 'package:my_app/app/app.dart';
 import 'package:my_app/bootstrap.dart';
+import 'package:my_app/firebase_options.dart';
 
-void main() {
-  bootstrap(() => const App());
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  await bootstrap(() => const App());
 }
